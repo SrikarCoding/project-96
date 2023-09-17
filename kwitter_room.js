9731f72e5ef5db370098f5e6d793da611b164d31
SrikarCoding <132314237+SrikarCoding@users.noreply.github.com>
2023-09-16 20:58:53 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,15 @@
+function addRoom(){
+    room_name = document.getElementById("room_name").value;
+firebase.database().ref("/").child(room_name).update({
+    purpose : "adding room name"
+});
+localStorage.setItem("room_name", room_name);
+window.location = "kwitter_page.html";
+}
+function getData(){
+    localStorage.setItem("room_name", room_name);
+    window.location = "kwitter_page.html";
+}
+function redirectToRoomName(){
+    localStorage.setItem("room_name", name);
+}
